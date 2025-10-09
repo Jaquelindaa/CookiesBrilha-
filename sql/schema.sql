0ee7505d9fd305e052ee867a02115281b6b7871c
Jaquelindaa <jaquelinemondini18@gmail.com>
2025-10-09 15:46:47 -0300

    Atualização SQL

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,9 +2,9 @@
 CREATE TABLE produto (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    preco NUMERIC(10, 2) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
     tipo VARCHAR(50) NOT NULL, -- Ex: 'Cookie Normal', 'Cookie Adicional', 'Bebida', 'Resgatavel'
-    estoque INTEGER NOT NULL DEFAULT 0,
+    estoque INT NOT NULL DEFAULT 0,
     custo_pontos INTEGER DEFAULT 0 -- 0 para produtos normais, > 0 para resgatáveis
 );
 
@@ -12,19 +12,21 @@ CREATE TABLE produto (
 CREATE TABLE cliente (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(14) UNIQUE NOT NULL,
-    telefone VARCHAR(20),
+    cpf VARCHAR(11) UNIQUE NOT NULL,
+    telefone VARCHAR(15),
     pontos_acumulados INTEGER NOT NULL DEFAULT 0
 );
 
 -- Tabela de Vendas
 CREATE TABLE venda (
     id SERIAL PRIMARY KEY,
+	cliente_id INT,
     data_venda TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     cliente_id INTEGER REFERENCES cliente(id), -- Nullable, se a venda for anônima
-    valor_total NUMERIC(10, 2) NOT NULL,
+    valor_total DECIMAL(10, 2) NOT NULL,
     pontos_gerados INTEGER NOT NULL DEFAULT 0,
-    tipo_transacao VARCHAR(20) NOT NULL -- 'VENDA' ou 'RESGATE'
+    tipo_transacao VARCHAR(10) NOT NULL, -- 'VENDA' ou 'RESGATE'
+	FOREIGN KEY (cliente_id) REFERENCES cliente(id)
 );
 
 -- Tabela de Itens da Venda (Para múltiplos produtos na venda)
@@ -33,6 +35,8 @@ CREATE TABLE item_venda (
     venda_id INTEGER REFERENCES venda(id) ON DELETE CASCADE,
     produto_id INTEGER REFERENCES produto(id),
     quantidade INTEGER NOT NULL,
-    preco_unitario NUMERIC(10, 2) NOT NULL,
-    subtotal NUMERIC(10, 2) NOT NULL
+    preco_unitario DECIMAL(10, 2) NOT NULL,
+    subtotal DECIMAL(10, 2) NOT NULL,
+	OREIGN KEY (venda_id) REFERENCES venda(id),
+    FOREIGN KEY (produto_id) REFERENCES produto(id)
 );
